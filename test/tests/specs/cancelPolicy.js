adcf943ee213eca48550fff9e527dd4317fe6039
arystan.akshulakov <a.akshulakov@a-i.kz>
2026-02-27 09:39:10 +0500

    fix: cancel policy only if it's not already cancelled

--- test/tests/specs/cancelPolicy.js
+++ test/tests/specs/cancelPolicy.js
@@ -4,13 +4,18 @@ const cascoAPI = require('../API/cascoAPI');
 
 exports.cancelPolicy = function () { // eslint-disable-line func-names
   it('cancel policy', async function () { // eslint-disable-line func-names
-    const cancelPolicyResponse = await cascoAPI.cancelPolicy(this.policyId);
-    cancelPolicyResponse.status.should.be.equal(200);
-    cancelPolicyResponse.data.should.containSubset(
-      JSONLoader.templateResponse.cancelPolicy,
-    );
-    cancelPolicyResponse.data.data.status.id
-      .should.be.equal(JSONLoader.dictCasco.policy_status.cancelled);
+    const policyResponse = await cascoAPI.getPolicy(this.policyId);
+    const policyStatus = policyResponse.data.data.status.id;
+
+    if (policyStatus !== JSONLoader.dictCasco.policy_status.cancelled) {
+      const cancelPolicyResponse = await cascoAPI.cancelPolicy(this.policyId);
+      cancelPolicyResponse.status.should.be.equal(200);
+      cancelPolicyResponse.data.should.containSubset(
+        JSONLoader.templateResponse.cancelPolicy,
+      );
+      cancelPolicyResponse.data.data.status.id
+        .should.be.equal(JSONLoader.dictCasco.policy_status.cancelled);
+    }
 
     await TWBHelpers.startSetPolicyWaiting();
     await TWBHelpers.verifyCancelledPolicy(this.policyNumber);
